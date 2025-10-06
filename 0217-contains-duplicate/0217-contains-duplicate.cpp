12211ba1fa5ca9ec50c725b9c3ac700659aeca38
UdaySinghBisht4112006 <udaybisht2006@gmail.com>
2025-10-06 19:26:30 +0530

    Time: 38 ms (72.28%), Space: 70.9 MB (87.82%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,13 +4,13 @@ using namespace std;
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); // Step 1: Sort the array
+        sort(nums.begin(), nums.end()); 
 
         for (int i = 0; i < nums.size() - 1; i++) {
             if (nums[i] == nums[i + 1]) 
                 return true;
         }
 
-        return false; //
-    }
+        return false; 
+            }
 };
